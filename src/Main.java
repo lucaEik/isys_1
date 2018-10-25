cf910628931ea886ca411c0ac1428be8480122e7
Crischinaldo <temp566566@gmx.de>
2018-10-25 11:29:08 +0200

    Chart

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+
 public class Main {
     //TODO: implement solution for changing values
     public static void main(String args[]) {
+
+        SwingUtilities.invokeLater(() -> {
+            LineChart example = new LineChart("Average days for People to change Opinion");
+            example.setAlwaysOnTop(true);
+            example.pack();
+            example.setSize(600, 400);
+            example.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            example.setVisible(true);
+        });
 
         Group g = new Group();
 
         System.out.print("---- Independent Simulation ----" + "\n");
 
-        g.iterateSimulateIndependent(2000, 0.022);
+        //g.iterateSimulateIndependent(20, 0.0222);
 
 
         System.out.print("\n"+"---- dependent Simulation ----" + "\n");
 
-        g.iterateSimulateDependent(2000, 0.00442);
+       // g.iterateSimulateDependent(20, 0.00442);
 
     }
 }
